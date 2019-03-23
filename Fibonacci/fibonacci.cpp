dc2f06db37fae2a8f4263b271b0e0cfe016a6bcc
Paul <aa_paul@outlook.com>
2019-03-23 10:11:33 +0800

    first commit

---
+++ Fibonacci/fibonacci.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//
+//using namespace std;
+//#include <utility>
+//
+//float fib(float n)
+//{
+//    if (n>2)
+//    {
+//        return fib(n-1)+fib(n-2);
+//    }
+//    else if (n<1)
+//    {
+//        cout<<"Error!"<<endl;
+//        return -1;
+//    }
+//    else
+//        return n-1;
+//}
+//
+//int main()
+//{
+//    int n;
+//    cin>>n;
+//    cout<<fib(n)<<endl;
+//    return 0;
+//}
+
+#include <utility>
+#include <stdint.h>
+#include <iostream>
+using namespace std;
+std::pair<uint64_t, uint64_t> Fib(size_t n)
+{
+  // 返回F_{n}和F_{n + 1}, 注意是对UINT64_MAX取模的结果.
+  if (n > 0)
+  {
+    auto PF = Fib(n / 2);   // 基于递归求解.
+    auto t0 = PF.first;
+    auto t1 = PF.second;
+    if (n % 2 == 1)         // 奇数情况.
+      return {t0 * t0 + t1 * t1, (2 * t0 + t1) * t1};
+    else
+      return {(2 * t1 - t0) * t0, t0 * t0 + t1 * t1};
+  }
+  return {0, 1};            // 基础情形: F_0和F1.
+}
+

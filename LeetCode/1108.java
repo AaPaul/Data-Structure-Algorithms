5fac90ed5982ac616c257a272d349e6386dd8bbb
Paul <aapaulcn@gmail.com>
2020-01-30 18:27:27 -0500

    Create 1108.java

---
+++ LeetCode/1108.java
@@ -0,0 +1,35 @@
+/*
+replace() doesn't support regular expressions
+replaceAll & replaceFrist support.
+
+Java String class has some functions like split which is same as the python version.
+/*
+
+class Solution {
+    public String defangIPaddr(String address) {
+        // String [] str = address.split("\\.");
+        return address.replace(".", "[.]");
+    }
+}
+
+// Java String class cannot be changed if it is be created
+
+//StringBuffer is safer than StringBuild while the latter one is faster than the former one. Both of them can be modified after being created.
+
+// This solution spend less than 1 ms to get the answer.
+class Solution2 {
+    public String defangIPaddr(String address) {
+        // String [] str = address.split("\\.");
+        char[] cr = address.toCharArray();
+        StringBuilder sb= new StringBuilder(cr.length); //Note: dont need to add "()" to get the length of array.
+        for (int i = 0; i < cr.length; i++) {
+            if (cr[i] == '.') {
+                sb.append("[.]");
+            }
+            else {
+                sb.append(cr[i]);
+            }
+        }
+        return sb.toString();
+    }
+}

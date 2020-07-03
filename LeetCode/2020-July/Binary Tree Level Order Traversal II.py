cd75e8f63c9282ba2e009d8ec6972aaf6a643c79
Ao <aapaulcn@gmail.com>
2020-07-02 20:58:13 -0400

    Update

---
+++ LeetCode/2020-July/Binary Tree Level Order Traversal II.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+from typing import List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        queue = []
+        searched = []
+        res = []
+        queue.append(root)
+        while queue:
+            res.append([q.val for q in queue])
+            queue = [kid for q in queue for kid in (q.left, q.right) if kid]
+            # node = queue.pop()
+            # if not node.val in searched:
+            #     searched.append(node.val)
+            # if node.left:
+            #     queue.insert(0, node.left)
+            #     temp.append(node.left.val)
+            # if node.right:
+            #     queue.insert(0, node.right)
+            #     temp.append(node.right.val)
+            # if temp:
+            #     res.insert(0, temp)
+        return list(reversed(res))
+
+
+def main():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = None
+    root.right.left = None
+    root.right.right = TreeNode(5)
+    s1 = Solution()
+    res = s1.levelOrderBottom(root)
+    print (res)
+
+if __name__ == '__main__':
+    main()

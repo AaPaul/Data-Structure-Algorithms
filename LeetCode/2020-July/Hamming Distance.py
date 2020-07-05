e9a36570ff5196f85d45518f9b6ce401f3f8f7a1
AaPaul <aa_paul@outlook.com>
2020-07-05 14:14:41 -0400

    update some meaning of signs

--- LeetCode/2020-July/Hamming Distance.py
+++ LeetCode/2020-July/Hamming Distance.py
@@ -25,7 +25,8 @@ class Solution:
             x >>= 1
         return res
 
-    def hammingDistance(self, x: int, y: int) -> int:
+    # Implement by myself
+    def hammingDistance2(self, x: int, y: int) -> int:
         n_max, n_min = max(x, y), min(x, y)
         b_max, b_min = self.toBin(n_max), self.toBin(n_min)
         count = 0
@@ -39,8 +40,25 @@ class Solution:
 
         return count
 
+    # Method 3
+    def hammingDistance(self, x: int, y: int) -> int:
+        res = x^y
+        res = bin(res)
+        return res.count('1')
 
 
 s1 = Solution()
 print(s1.hammingDistance(1501377268, 258791155))
 # print(s1.hammingDistance(1, 8))
+
+
+"""
+& 是按位与运算符，5&4输出4，表示为0000 0010
+参与运算的两个值,如果两个相应位都为1,则该位的结果为1,否则为0
+5：0101
+4：0100
+
+^ 按位异或运算符：当两对应的二进位相异时，结果为1
+5：0101
+4：0100
+"""

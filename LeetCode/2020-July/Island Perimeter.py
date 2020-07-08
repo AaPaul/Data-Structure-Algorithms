4f2611a81bc1158b49c4c6e05e421e0a8974da5e
Ao <aapaulcn@gmail.com>
2020-07-07 22:47:07 -0400

    updates

--- LeetCode/2020-July/Island Perimeter.py
+++ LeetCode/2020-July/Island Perimeter.py
@@ -5,52 +5,63 @@ class Solution:
         if not grid:
             return 0
         
-        def DFS(grid, x, y) -> int:
-            grid[x][y] = -1
+        def DFS(grid, x:int, y:int) -> int:
+            # grid[x][y] = -1
+            visted[x][y] = 1
             num = 4
             # Up
             if x-1 >= 0:
-                if grid[x-1][y] == 1:
+                if grid[x-1][y] == 1 and not visted[x-1][y]:
                     num = num + DFS(grid, x-1, y) - 1
-                if grid[x-1][y] == -1:
+                # if grid[x-1][y] == -1:
+                elif visted[x-1][y]:
                     num -= 1
             
 
             # Right
             if y+1 < len(grid[0]):
-                if grid[x][y+1] == 1:
+                if grid[x][y+1] == 1 and not visted[x][y-1]:
                     num = num  + DFS(grid, x, y+1) - 1
-                if grid[x][y+1] == -1:
+                # if grid[x][y+1] == -1:
+                elif visted[x][y+1]:
                     num -= 1
             
 
             # Down
             if x+1 < len(grid):
-                if grid[x+1][y] == 1:
+                if grid[x+1][y] == 1 and not visted[x+1][y]:
                     num = num + DFS(grid, x+1, y) - 1
                 # if grid[x+1][y] == -1:
-                #     num -= 1
+                elif visted[x+1][y]:
+                    num -= 1
 
             # Left
             if y-1 >= 0:
-                if grid[x][y-1] == 1:
+                if grid[x][y-1] == 1 and not visted[x][y-1]:
                     num = num + DFS(grid, x, y-1) - 1
                 # if grid[x][y-1] == -1:
-                #     num -= 1
+                elif visted[x][y-1]:
+                    num -= 1
 
             return num
 
 
         # res = [[0] * len(grid[0])] * len(grid)
-        # res = 0
-        res = DFS(grid,0,0)
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[0])):
-        #         if grid[i][j] == 1:
-        #             res += DFS(grid, i, j)
+        res = 0
+        visted = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+        # res = DFS(grid,0,0)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1 and not visted[i][j]:
+                    res += DFS(grid, i, j)
 
         return res
     
 s1 = Solution()
-# print(s1.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
+print(s1.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
 print(s1.islandPerimeter([[1, 1], [1, 1]]))
+print(s1.islandPerimeter([[1, 1, 0]]))
+print(s1.islandPerimeter([[1, 0], [1, 0], [1, 0]]))
+
+# https://blog.csdn.net/nxhyd/article/details/72325783
+# https://blog.csdn.net/zxzxzx0119/article/details/82966388

2cb43469b2daffa63bcb065e9012b5720c39a4ef
Ao <aapaulcn@gmail.com>
2020-07-16 19:42:07 -0400

    Update

--- LeetCode/2020-July/Pow(x, n).py
+++ LeetCode/2020-July/Pow(x, n).py
@@ -18,21 +18,23 @@ class Solution:
         
         return square(x, n)
 
-        # This method is not good enough especially for n which has a big value 
-        def myPow3(self, x, n):
-            if n<0:
-                x = 1 / x
-                n = -n
-            res = 1
-            while n:
-                res *= x
-                n -= 1
-                if abs(res) < 1e-7:
-                    break
+    # This method is not good enough especially for n which has a big value 
+    # For redundency part, like 1^n, the 2nd method can easily solve it.
+    def myPow3(self, x, n):
+        if n<0:
+            x = 1 / x
+            n = -n
+        res = 1
+        while n:
+            res *= x
+            n -= 1
+            if abs(res) < 1e-7:
+                break
 
-            return res
+        return res
 
 s1 = Solution()
 print(s1.myPow3(-2, -2))
 print(s1.myPow(-2, -2))
+print(s1.myPow3(1, 2147483647))
 # 

13e2c4452f5672edc81966622f3e62c23f2f9984
Ao <aapaulcn@gmail.com>
2020-08-12 17:20:05 -0400

    Update

---
+++ LeetCode/2020-August/Pascal's Triangle II.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        if rowIndex == 1:
+            return [1,1]
+        temp = [1,1]
+        for i in range(2, rowIndex+1):
+            res = [1]
+            for j in range(1, len(temp)):
+                res.append(temp[j-1]+temp[j])
+            res.append(1)
+            temp = res
+        return res

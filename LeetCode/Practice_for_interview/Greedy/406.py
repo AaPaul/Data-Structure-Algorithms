e2d9e0e583576738f35bac5ccd0889db1fc5ad4c
AaPaul <aapaulcn@gmail.com>
2021-10-31 16:20:23 -0400

    greedy

---
+++ LeetCode/Practice_for_interview/Greedy/406.py
@@ -0,0 +1,25 @@
+"""
+The meaning of list[i:j] = [x]
+if i == j, list[i:j] = [x] behaves like list.insert(j, x)
+if i == j == len(list), list.append(x)
+if i != j,  then slice of list from i to j is replaced by the contents of the iterable [x].
+
+
+https://stackoverflow.com/questions/59060606/in-python-why-is-listii-n-insert-an-element-in-listii
+"""
+from typing import List
+
+
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        
+
+
+
+
+
+
+
+
